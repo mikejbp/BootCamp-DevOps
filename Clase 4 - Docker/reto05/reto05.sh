ba7156c934c5556ff613f32f81253cd32b46e843
mikejbp <migueljosebp@gmail.com>
2022-08-11 21:35:40 -0400

    ADD: script del reto05

---
+++ Clase 4 - Docker/reto05/reto05.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Author: Miguel Briceno\
+
+# Script para containerizar aplicacion con Node.js
+
+# Creacion de la imagen
+docker build -t hello-bootcamp .
+docker images
+
+# Creacion del contenedor con la imagen previa
+docker run --name reto05 -d -p 4000:4000 hello-bootcamp:latest
+docker ps
+
+# Subir al repositorio en DockerHub
+docker login
+docker tag hello-bootcamp mikejbp/reto05docker:latest
+docker push mikejbp/reto05docker:latest
+
+exit
